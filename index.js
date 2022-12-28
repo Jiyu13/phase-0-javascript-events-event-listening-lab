5a33d99aac4123ccc175bf0ac99bb2aef1d25761
Ziru Zou <jiyucoding@yahoo.com>
2022-12-28 10:52:46 -0500

    commit tree 63b743175d572a7dd5df202682ff0a412e25c4bd

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// copy the code into the index.js file's addingEventListener() function and run the test
+
 function addingEventListener() {
+    let input = document.getElementById('button');
+
+    function clickAlert() {
+        alert('I was clicked!')
+    }
+
+    input.addEventListener('click', clickAlert);
 }
+
+
+// call the addingEventListener()
+console.log(addingEventListener());
